79df042a421d643dcca47316e1d3db2753bc63a2
Bryan Chabuka <hbbchabuka@gmail.com>
2025-03-01 10:20:01 +0200

    Update simple_calculator.py

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,17 +1,25 @@
 def multiplication(var1, var2):
     return var1*var2
-
 def division(var1, var2):
     if var2 == 0:
         return 'Error: Divisionby zero is not allowed'
     return var1/var2
-
 def addition(var1, var2):
     return var1+var2
-
 def subtraction(var1,var2):
     return var1-var2
-
+def square(var1):
+    return var1*var1
+def square_root(var1):
+    if var1 < 0:
+        return 'Error: Square root of a negative number is not allowed'
+    return var1**0.5
+def power(var1,var2):
+    return pow(var1,var2)
+def root(var1,var2):
+    if var1 < 0 and var2%2 == 0:
+        return 'Error: Root of a negative number is not allowed'
+    return pow(var1,1/var2)
 def num_receiver():
     converting = True
     while converting:
